7d7f3463920f838233db8449cb45ae2e417ae6e2
dazhen <dengdazhen@qq.com>
2020-06-07 19:20:36 +0800

    minio + gallery

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import imgData from './data/visimages_data.json'
+import authorNames from './data/authors.json'
+import paperInfo from './data/visimage_paper_info.json'
+
 
 const drawerWidth = 350;
 
@@ -28,24 +32,33 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
     flexShrink: 0,
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: 'auto',
-  },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
   },
 }));
 
-export default function ClippedDrawer() {
+export default function App() {
   const classes = useStyles();
+  let imgList = [];
+  let paperList = [];
+  for (const paper in imgData) {
+    for (const img in imgData[paper]) {
+      imgList.push({
+        imgName:imgData[paper][img]["file_name"],
+      })
+    };
+    paperList.push(paperInfo[paper]['title'])
+  }
+  let init_data = {
+    year:[1990,2018],
+    authors:authorNames,
+    imgList:imgList,
+    paperList:paperList,
+  };
+  // console.log(init_data)
+  // for (idx = 0; i < )
+
 
   return (
     <div className={classes.root}>
@@ -59,11 +72,11 @@ export default function ClippedDrawer() {
       </AppBar>
       <div className={classes.controlPanel}>
       <Toolbar/>
-        <ControlPanel/>
+        <ControlPanel init_data = {init_data}/>
       </div>
       <main className={classes.content}>
         <Toolbar />
-        <ImageGallery/>
+        <ImageGallery imgList={imgList}/>
       </main>
     </div>
   );
--- src/component/ImageGallery.js
+++ src/component/ImageGallery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,32 +8,76 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+// import Minio from 'minio'
 
-const useStyles = makeStyles({
+
+var Minio = require('minio')
+
+var minioClient = new Minio.Client({
+  endPoint: 'minio.zjvis.org',
+  accessKey: 'ZWUkouAYfWR5ycxc1LL8Mbp0d02yqDnrJHSKh0sN0javzWZ79KcIVY53ns78pCYW',
+  secretKey: 'l5sKx2Bmuqv308ZtzkhaPSlgscUezpJrtBOH1mICnKCW6FfrCPZa6KDrV8zp5aAM#'
+});
+
+// console.log(minioClient.getObject('visdata', 'images/1032/3.png'))
+
+var res = minioClient.presignedUrl('GET',
+  'visdata', 'images/1032/3.png', 24 * 60 * 60, function (err, presignedUrl) {
+    if (err) return console.log(err)
+    return presignedUrl
+  })
+
+console.log(res)
+
+const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   card: {
     maxWidth: 250,
-    margin:5,
+    margin: 5,
   },
   media: {
     height: 140,
   },
 });
 
-export default function ImageGallery() {
-  const classes = useStyles();
-  const elements = ['one', 'two', 'three'];
-  return (
-    <Grid container justify="space-between" spacing={50}>
-      {[...Array(50).keys()].map((value) => (
+class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { urls: [] };
+  }
+
+  componentDidMount() {
+    // var temurls = []
+    // console.log(this.props.imgList.length)
+    for (var i = 1250; i < 1300; i++) {
+      let [paperId, imgId] = this.props.imgList[i].imgName.split('.')[0].split('_')
+      console.log(paperId, imgId)
+      paperId = parseInt(paperId)
+      imgId = parseInt(imgId)
+      minioClient.presignedUrl('GET', 'visdata', `images/${paperId}/${imgId}.png`, 24 * 60 * 60,
+        (err, presignedUrl) => {
+          const {urls} = this.state;
+          urls.push(presignedUrl)
+          this.setState({ urls: urls })
+
+        })
+    }
+    // console.log("temp",temurls)
+
+  }
+  render() {
+    const {classes} = this.props;
+    const {urls} = this.state;
+
+    return (<Grid container justify="space-between" spacing={50}>
+      {urls.map((value) => (
         <Card className={classes.card}>
           <CardActionArea>
             <CardMedia
               className={classes.media}
-              image="/static/corgi.jpg"
-              title="Contemplative Reptile"
+              image={value}
             />
           </CardActionArea>
           <CardActions>
@@ -46,6 +90,8 @@ export default function ImageGallery() {
           </CardActions>
         </Card>
       ))}
-    </Grid>
-  );
+    </Grid>)
+  };
 }
+
+export default withStyles(styles)(ImageGallery);
