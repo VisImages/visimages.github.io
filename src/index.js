8ae5453440f52127c309c9290691b6c598a69e12
dazhen <dengdazhen@qq.com>
2020-06-08 21:33:28 +0800

    minor

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from "mobx";
+import {} from "./store";
 
 ReactDOM.render(<App />, document.querySelector('#root'));
