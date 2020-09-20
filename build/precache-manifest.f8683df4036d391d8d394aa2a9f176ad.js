80c88f48c2cb3fbfc524e3239ce06b84de3d559b
dazhen <dengdazhen@outlook.com>
2020-09-21 01:39:21 +0800

    build

---
+++ build/precache-manifest.f8683df4036d391d8d394aa2a9f176ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d08b6b89d760d67d5c2cf7521c66f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe46ccd954795d8ec582",
+    "url": "/static/css/main.9daa7be1.chunk.css"
+  },
+  {
+    "revision": "2612592d9ecfeee7c57b",
+    "url": "/static/js/2.185b98a9.chunk.js"
+  },
+  {
+    "revision": "9110c923800987d12d856da8548c4502",
+    "url": "/static/js/2.185b98a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe46ccd954795d8ec582",
+    "url": "/static/js/main.1b7c7c86.chunk.js"
+  },
+  {
+    "revision": "d439d549201e27139909",
+    "url": "/static/js/runtime-main.abcabe74.js"
+  },
+  {
+    "revision": "62a8d7cab7da7ba87db62bb75e8ade08",
+    "url": "/static/media/logo.62a8d7ca.svg"
+  }
+]);
